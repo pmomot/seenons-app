ff3e73af02dc714fb991a326f1aa5eb842f485c2
Petro Momot <petruha.momot@gmail.com>
2021-11-07 13:49:32 +0100

    feat: add back arrow, improve some styling

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,12 @@
 <template>
-  <div class="background-gradient"></div>
+  <div class="fixed inset-0 -z-1 w-screen h-screen background-gradient"></div>
   <TopNav />
   <router-view />
 </template>
 
 <style>
-/*#app {*/
-/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
-/*  -webkit-font-smoothing: antialiased;*/
-/*  -moz-osx-font-smoothing: grayscale;*/
-/*  text-align: center;*/
-/*  color: #2c3e50;*/
-/*}*/
-
 .background-gradient {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
   background: linear-gradient(42deg, #46607f, #16a891 80%, #57c595);
-  z-index: -1;
-}
-
-#temp-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#temp-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
 
